1ae780faff8a79831f69b4ee5c6f461974a98087
Eder Silva <contato@edersilva.com>
2024-10-27 16:57:14 -0300

    Vercel

--- app/settings.py
+++ app/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://workshops.edersilva.com',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'https://vercel.app',
 ]
 
 # Application definition
