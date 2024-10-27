7174be620a7549cc3ecc890f758d2280e31bab8d
Eder Silva <contato@edersilva.com>
2024-10-27 01:05:07 -0300

    Added nginx server with Docker

--- app/settings.py
+++ app/settings.py
@@ -131,8 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/app/staticfiles'
+
+# Adicione esta linha para incluir os arquivos estáticos do admin
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
